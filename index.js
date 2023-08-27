6c81f07642c99804f47b19187165df6c3d888bf1
tblan21 <tmb2225@gmail.com>
2023-08-27 16:57:28 -0500

    add event listener for input form

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function app() {
+    
+    let addTask = document.querySelector('.add');
+
+    addTask.addEventListener("click", function() {
+        let list = document.querySelector('.list');
+        
+        let li = document.createElement('li');
+        let input = document.querySelector('.task').value;
+}
+
 app();
